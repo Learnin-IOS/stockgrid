967690003620193102ffc5196f48cf709d141d6f
Sharkzuria <baumabulondo@gmail.com>
2023-03-09 21:15:18 +0300

    [chore] Update Utils

--- stockgrid/Utils.swift
+++ stockgrid/Utils.swift
@@ -5,11 +5,11 @@
 //  Created by Le Bon B' Bauma on 07/03/2023.
 //
 
-import Foundation
-
-#if DEBUG
-extension struct Utils {
-    
-}
-
-#endif
+//import Foundation
+//
+//#if DEBUG
+//extension struct Utils {
+//    
+//}
+//
+//#endif
