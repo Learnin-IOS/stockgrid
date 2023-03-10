bf308eb2645893fb2437671e83ed77eace4f9bce
Sharkzuria <baumabulondo@gmail.com>
2023-03-10 17:44:59 +0300

    [chore] Implement Number Formatter for the currency

--- stockgrid/Utils.swift
+++ stockgrid/Utils.swift
@@ -5,11 +5,25 @@
 //  Created by Le Bon B' Bauma on 07/03/2023.
 //
 
-//import Foundation
-//
-//#if DEBUG
-//extension struct Utils {
-//    
-//}
-//
-//#endif
+import Foundation
+
+
+struct Utils {
+    
+    static let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = ""
+        formatter.currencyDecimalSeparator = ","
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    static func format(value: Double?) -> String? {
+        guard let value,
+              let text = numberFormatter.string(from: NSNumber(value: value))
+        else { return nil }
+        return text
+    }
+}
