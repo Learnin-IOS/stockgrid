a377e3cb7eced13c97a87babaa6e6584496a2559
Sharkzuria <baumabulondo@gmail.com>
2023-03-12 21:21:17 +0300

    [chore] Implementing Quote extensions to format regular Market Change

--- stockgrid/Extensions/Quote+Extensions.swift
+++ stockgrid/Extensions/Quote+Extensions.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import StocksAPI
+
+extension Quote {
+    
+    var regularPriceText: String? {
+        Utils.format(value: regularMarketPrice)
+    }
+    
+    var regularDiffText: String? {
+        guard let text = Utils.format(value: regularMarketChange) else { return nil }
+        return text.hasPrefix("-") ? text : "+\(text)"
+    }
+    
+}
