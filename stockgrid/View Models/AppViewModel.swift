2c5f067881d8e8de3c5dae58a723758d2539acb9
Sharkzuria <baumabulondo@gmail.com>
2023-03-15 02:19:23 +0300

    [chore] Update AppViewModel
    Here we update the view model in order it can implement the Main List View Body & Preview

--- stockgrid/View Models/AppViewModel.swift
+++ stockgrid/View Models/AppViewModel.swift
@@ -15,4 +15,33 @@ class AppViewModel: ObservableObject {
     
     @Published var tickers: [Ticker] = []
     
+    
+    var titleText = "Stockgrid"
+    @Published var subtitleText: String
+    var emptyTickersText = "Search & Add symbol to see stock quotes"
+    var attributionText = "Powered by Yahoo! Finance API"
+    
+    
+    
+    private let subtitleDateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateFormat = "d MMM"
+        return df
+    }()
+    
+    init() {
+        self.subtitleText = subtitleDateFormatter.string(from: Date())
+    }
+    
+    
+    
+    func removeTickers(atOffSets offsets: IndexSet) {
+        tickers.remove(atOffsets: offsets)
+    }
+     
+    func openYahooFinance() {
+        let url = URL(string: "https://finance.yahoo.com")!
+        guard UIApplication.shared.canOpenURL(url) else  { return }
+        UIApplication.shared.open(url)
+    }
 }
