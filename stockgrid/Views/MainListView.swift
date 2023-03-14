3cbaa52e021c75a045bda23c3298f9bfc8ed8a1b
Sharkzuria <baumabulondo@gmail.com>
2023-03-14 22:13:23 +0300

    [chore] Quotes Extension & Stubs - Main List View

--- stockgrid/Views/MainListView.swift
+++ stockgrid/Views/MainListView.swift
@@ -9,19 +9,64 @@ import SwiftUI
 import StocksAPI
 
 struct MainListView: View {
+    
+    // MARK: - Properties
+    @EnvironmentObject var appVM: AppViewModel
+    @StateObject var quotesVM = QuotesViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        tickerListView
+    }
+    
+    private var tickerListView: some View {
+        List {
+            ForEach(appVM.tickers) { ticker in
+                TickerListRowView(
+                    data: .init(
+                        symbol: ticker.symbol,
+                        name: ticker.shortname,
+                        price: quotesVM.priceForTicker(ticker ),
+                        type: .main))
+                
+            }
         }
-        .padding()
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
+    
+    @StateObject static var appVM: AppViewModel = {
+        let vm = AppViewModel()
+        vm.tickers = Ticker.stubs
+        return vm
+    }()
+    
+    @StateObject static var emptyAppVM: AppViewModel = {
+        let vm = AppViewModel()
+        vm.tickers = []
+        return vm
+    }()
+    
+    @StateObject static var quotesVM: QuotesViewModel = {
+        let vm = QuotesViewModel()
+        vm.quotesDict = Quote.stubsDict
+        return vm
+        
+    }()
+    
     static var previews: some View {
-        MainListView()
+        Group {
+            NavigationStack{
+                MainListView(quotesVM: quotesVM)
+            }
+            .environmentObject(appVM)
+            .previewDisplayName("With Tickers")
+            
+            NavigationStack{
+                MainListView(quotesVM: quotesVM)
+            }
+            .environmentObject(emptyAppVM)
+            .previewDisplayName("With Empty Tickers")
+        }
     }
 }
