e6031c46e76cab952cbb338ec974608400b8e394
Sharkzuria <baumabulondo@gmail.com>
2023-03-16 00:15:01 +0300

    [feat] creating and implementing stubs for StocksAPI

---
+++ stockgrid/Mocks & Stubs/MockStoksAPI.swift
@@ -0,0 +1,28 @@
+//
+//  MockStoksAPI.swift
+//  stockgrid
+//
+//  Created by Le Bon B' Bauma on 15/03/2023.
+//
+
+import Foundation
+import StocksAPI
+
+#if DEBUG
+    
+struct MockStocksAPI: StockAPI {
+    
+    var stubbedSearchTickersCallback: (() async throws -> [Ticker])!
+    func searchTickers(query: String, isEquityTypeOnly: Bool) async throws -> [Ticker] {
+        try await stubbedSearchTickersCallback()
+    }
+    
+    var stubbedFetchQuotesCallback: (() async throws -> [Quote])!
+    func fetchQuotes(symbols: String) async throws -> [Quote] {
+        try await stubbedFetchQuotesCallback()
+    }
+    
+    
+}
+
+#endif
