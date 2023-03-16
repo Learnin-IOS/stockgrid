a07ef47d87a72ead82a97ecaf5fedf20c042d46f
Sharkzuria <baumabulondo@gmail.com>
2023-03-16 07:21:43 +0300

    [chore] Implement TickerListRepository to App View Model

--- stockgrid/Views/SearchView.swift
+++ stockgrid/Views/SearchView.swift
@@ -81,9 +81,9 @@ struct SearchView_Previews: PreviewProvider {
     }()
     
     @StateObject static var appVM: AppViewModel = {
-        let vm = AppViewModel()
-        vm.tickers = Array(Ticker.stubs.prefix(upTo: 2))
-        return vm
+        var mock = MoockTickerListRepository()
+        mock.stubbedLoad = { Array(Ticker.stubs.prefix(upTo: 2)) }
+        return AppViewModel(repository: mock)
     }()
     
     static var quotesVM: QuotesViewModel = {
