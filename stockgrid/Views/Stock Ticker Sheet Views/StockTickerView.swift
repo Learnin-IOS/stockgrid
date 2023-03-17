34be87d21c22e94eb7a0f16bc8fddf1cd174aeb9
Sharkzuria <baumabulondo@gmail.com>
2023-03-17 23:53:49 +0300

    [chore] Handle Data stream in Preview

--- stockgrid/Views/Stock Ticker Sheet Views/StockTickerView.swift
+++ stockgrid/Views/Stock Ticker Sheet Views/StockTickerView.swift
@@ -9,13 +9,71 @@ import SwiftUI
 import StocksAPI
 
 struct StockTickerView: View {
+    @StateObject var quoteVM: TickerQuoteViewModel
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct StockTickerView_Previews: PreviewProvider {
+    
+    static var tradingStubsQuoteVM: TickerQuoteViewModel  = {
+        var mockAPI = MockStocksAPI()
+        mockAPI.stubbedFetchQuotesCallback = {
+            [Quote.stub(istrading: true)]
+        }
+        return TickerQuoteViewModel(ticker: .stub, stockAPI: mockAPI)
+    }()
+    
+    static var closedStubsQuoteVM: TickerQuoteViewModel  = {
+        var mockAPI = MockStocksAPI()
+        mockAPI.stubbedFetchQuotesCallback = {
+            [Quote.stub(istrading: false)]
+        }
+        return TickerQuoteViewModel(ticker: .stub, stockAPI: mockAPI)
+    }()
+    
+    static var loadingStubsQuoteVM: TickerQuoteViewModel  = {
+        var mockAPI = MockStocksAPI()
+        mockAPI.stubbedFetchQuotesCallback = {
+            await withCheckedContinuation{ _ in
+                
+            }
+        }
+        return TickerQuoteViewModel(ticker: .stub, stockAPI: mockAPI)
+    }()
+    
+    static var errorStubsQuoteVM: TickerQuoteViewModel  = {
+        var mockAPI = MockStocksAPI()
+        mockAPI.stubbedFetchQuotesCallback = {
+            throw NSError(domain: "error", code: 0, userInfo: [NSLocalizedDescriptionKey : "An Error has Occured"])
+        }
+        return TickerQuoteViewModel(ticker: .stub, stockAPI: mockAPI)
+    }()
     static var previews: some View {
-        StockTickerView()
+        Group {
+            StockTickerView(quoteVM: tradingStubsQuoteVM)
+                .previewDisplayName("Trading")
+                .frame(height: 700)
+            
+            StockTickerView(quoteVM: closedStubsQuoteVM)
+                .previewDisplayName("Closed")
+                .frame(height: 700)
+            
+            StockTickerView(quoteVM: loadingStubsQuoteVM)
+                .previewDisplayName("Loading")
+                .frame(height: 700)
+            
+            StockTickerView(quoteVM: errorStubsQuoteVM)
+                .previewDisplayName("Error")
+                .frame(height: 700)
+            
+           
+        }.previewLayout(.sizeThatFits)
+        
+        
+
     }
 }
