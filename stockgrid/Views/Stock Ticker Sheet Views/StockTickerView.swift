993910c1549c98cf54f2f175308f2dc09d200131
Sharkzuria <baumabulondo@gmail.com>
2023-03-17 23:09:18 +0300

    [chore] Create Stock Ticker Sheet View

---
+++ stockgrid/Views/Stock Ticker Sheet Views/StockTickerView.swift
@@ -0,0 +1,21 @@
+//
+//  StockTickerView.swift
+//  stockgrid
+//
+//  Created by Le Bon B' Bauma on 17/03/2023.
+//
+
+import SwiftUI
+import StocksAPI
+
+struct StockTickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StockTickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockTickerView()
+    }
+}
