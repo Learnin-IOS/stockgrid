4c48fddc3c23a964a717fccaea20ec9ad6089e38
Sharkzuria <baumabulondo@gmail.com>
2023-03-31 22:13:58 +0300

    [chore] Fetch chart data from API

--- stockgrid/Services/StockAPI.swift
+++ stockgrid/Services/StockAPI.swift
@@ -11,9 +11,9 @@ import StocksAPI
 protocol StockAPI {
     func searchTickers(query: String, isEquityTypeOnly: Bool) async throws -> [Ticker]
     func fetchQuotes(symbols: String) async throws -> [Quote]
-}
-
-
-extension StocksAPI: StockAPI {
+    func fetchChartData (symbol: String, range: ChartRange) async throws -> ChartData?
     
 }
+
+
+extension StocksAPI: StockAPI {}
